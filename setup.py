c6f65a8ed6c67661716b34a8847b48e1ea755121
425776024 <425776024@qq.com>
2020-06-17 11:50:27 +0800

    init

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding='UTF-8') as fh:
 
 setup(
     name="easyleetcode",
-    version="1.2.0",
+    version="1.3.0",
     keywords=("easyleetcode", "bilibili", 'youtube', 'leetcode'),
     description="Leetcode easy study use Python on web",
     long_description=long_description,
