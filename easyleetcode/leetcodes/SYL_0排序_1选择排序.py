c81aa5d396e2ba640b3012794d12750280102340
425776024 <425776024@qq.com>
2020-06-18 22:59:07 +0800

    v 1.9.2 window.open(url);

--- easyleetcode/leetcodes/SYL_0排序_1选择排序.py
+++ easyleetcode/leetcodes/SYL_0排序_1选择排序.py
@@ -1,1 +1,14 @@
-print('SYL_L_001_0排序_1选择排序')
+
+def Sort(arr):
+    for i in range(len(arr)):
+        min_i = i
+        for j in range(i, len(arr)):
+            if arr[j] < arr[min_i]:
+                min_i = j
+        arr[i], arr[min_i] = arr[min_i], arr[i]
+    return arr
+
+
+if __name__ == '__main__':
+    arr = Sort([3, 5, 1, 2, 6, 7, 9, 11])
+    print(arr)
