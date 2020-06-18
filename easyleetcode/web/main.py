ab370d89fcf20c3888d0dc36987e00cb03ccdd6b
425776024 <425776024@qq.com>
2020-06-18 22:45:02 +0800

    v 1.9.1 print note

--- easyleetcode/web/main.py
+++ easyleetcode/web/main.py
@@ -23,12 +23,17 @@ def run(host="127.0.0.1", port=8080, py_cmd_path='python '):
     :param py_cmd_path:python exe path/cmd
     :return:
     '''
+    print('your host is :', host)
+    print('your port is :', port)
+    print('your python execute path is :', py_cmd_path, '(use command:', py_cmd_path, ' xxx.py to run python)')
+
     config.templates.env.variable_start_string = '{['
     config.templates.env.variable_end_string = ']}'
     a_server.python_path = py_cmd_path
     # every times running,path config.count_day 's count +=1 (running count log)
     utils.add_file_txt_count(config.count_day)
-    print('_____running:____,please web browser', 'http://%s:%s' % (str(host), str(port)))
+    print('_____running:____')
+    print('please web browser', 'http://%s:%s' % (str(host), str(port)))
     # uvicorn.run(app=app, host=host, port=port, log_level="error")
     uvicorn.run(app=app, host=host, port=port)
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding='UTF-8') as fh:
 
 setup(
     name="easyleetcode",
-    version="1.9.0",
+    version="1.9.1",
     keywords=("easyleetcode", "bilibili", 'youtube', 'leetcode'),
     description="Leetcode easy study use Python on web",
     long_description=long_description,
